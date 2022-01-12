5d9ad36e6dee9fe55d4577bb8dacd705f037ee75
ipax77 <ph.ilipp@web.de>
2022-01-12 11:15:02 +0100

    refactoring / movequality

---
+++ src/pax.BlazorChess/Pages/EngineAnalyzesPage.razor.cs
@@ -0,0 +1,318 @@
+﻿using Microsoft.AspNetCore.Components;
+using pax.BlazorChess.Models;
+using pax.BlazorChess.Services;
+using pax.BlazorChess.Shared;
+using pax.chess;
+using pax.uciChessEngine;
+
+namespace pax.BlazorChess.Pages;
+public partial class EngineAnalyzesPage : ComponentBase, IDisposable
+{
+# nullable disable
+    [Inject]
+    protected EngineService engineService { get; set; }
+    [Inject]
+    protected DbService dbService { get; set; }
+    [Inject]
+    protected NavigationManager _nav { get; set; }
+    [Inject]
+    protected ILogger<EngineAnalyzesPage> logger { get; set; }
+# nullable enable
+
+    [Parameter]
+    [SupplyParameterFromQuery]
+    public int? GameId { get; set; }
+
+    List<GameAnalyzes> GameAnalyzis = new List<GameAnalyzes>();
+    GameAnalyzes? Analysis;
+    BoardContainer? boardContainer;
+    ChartComponent? chart;
+    LoadModal? loadModal;
+    SettingsModal? settingModal;
+    EngineComponent? engineComponent;
+
+    CancellationTokenSource cts = new CancellationTokenSource();
+
+    private Chart _chart = ChartService.GetRatingChart();
+    private bool Analyzing = false;
+
+    private ReviewSettings reviewSettings = new ReviewSettings();
+
+    private Dictionary<int, List<Variation>> ReviewVariations = new Dictionary<int, List<Variation>>();
+
+    private int currentMoveId => Analysis == null ? 0 : Analysis.Game.ObserverState.CurrentMove == null ? 0 : Analysis.Game.ObserverState.CurrentMove.Variation != null ? 0 : Analysis.Game.ObserverState.CurrentMove.HalfMoveNumber;
+    private List<Variation> reviewVariations = new List<Variation>();
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        reviewSettings.EngineString = engineService.AvailableEngines.First().Key;
+        _ = Init();
+    }
+
+    private async Task Init()
+    {
+        GameAnalyzis = engineService.GetGameAnalyses();
+        if (GameId == null)
+        {
+            Analysis = GameAnalyzis.FirstOrDefault();
+        }
+        else
+        {
+            var game = await dbService.GetGameFromIdAsync((int)GameId);
+            if (game != null)
+            {
+                Analysis = engineService.CreateGameAnalyzes(game, reviewSettings.EngineString);
+            }
+        }
+        await InvokeAsync(() => StateHasChanged());
+        if (Analysis != null)
+        {
+            _chart.data.labels = Analysis.Game.State.Moves.Select(s => s.HalfMoveNumber.ToString()).ToList();
+        }
+    }
+
+    private async void GameImport(Game game)
+    {
+        GameId = null;
+        Analysis = engineService.CreateGameAnalyzes(game, reviewSettings.EngineString);
+
+        if (game.State.Moves.Any())
+        {
+            if (!game.Infos.ContainsKey("UTCDate"))
+            {
+                game.Infos["UTCDate"] = DateTime.UtcNow.ToString("yyyy.MM.dd");
+                game.Infos["UTCTime"] = DateTime.UtcNow.ToString("HH:mm:ss");
+            }
+            var dbGame = await dbService.SaveGame(game, null);
+            if (dbGame != null)
+            {
+                GameId = dbGame.Id;
+                _nav.NavigateTo(_nav.GetUriWithQueryParameter("GameId", GameId));
+            }
+        }
+    }
+
+    private void SettingsChoosen()
+    {
+        if (Analysis != null)
+        {
+            engineService.DeleteGameAnalyzes(Analysis);
+            Analysis = engineService.CreateGameAnalyzes(Analysis.Game, reviewSettings.EngineString);
+        }
+    }
+
+    private void ChartClicked(string label)
+    {
+        if (Analysis != null)
+        {
+            int i;
+            if (int.TryParse(label, out i))
+            {
+                Analysis.Game.ObserverMoveTo(i);
+                ObserverMoveChanged();
+                boardContainer?.Focus();
+            }
+        }
+    }
+
+    private void UpdateChart()
+    {
+        if (Analysis != null)
+        {
+            _chart.data.datasets[0].data.Clear();
+            for (int i = 0; i < Analysis.Game.State.Moves.Count; i++)
+            {
+                if (ReviewVariations.ContainsKey(i) && ReviewVariations[i].Any())
+                {
+                    var chartScore = ReviewVariations[i].OrderBy(o => o.Pv).First().Evaluation?.ChartScore();
+                    _chart.data.datasets[0].data.Add(chartScore == null ? 0 : (double)chartScore);
+                } else
+                {
+                    _chart.data.datasets[0].data.Add(0);
+                }
+            }
+            chart?.UpdateDataset(_chart.data.datasets[0]);
+        }
+    }
+
+    private void ObserverMoveChanged()
+    {
+        if (Analysis != null && Analysis.Game.ObserverState.CurrentMove != null)
+        {
+            chart?.DrawHorizontalLine(Analysis.Game.ObserverState.CurrentMove.HalfMoveNumber);
+            boardContainer?.DrawReviewHints();
+
+            reviewVariations = Analysis.Game.GetReviewVariations();
+            // nextVariation = !Analysis.Game.ReviewVariations.ContainsKey(currentMoveId + 1) ? null : Analysis.Game.ReviewVariations[currentMoveId + 1].OrderBy(o => o.Pv).FirstOrDefault();
+        }
+        _ = engineComponent?.Update();
+    }
+
+    private MoveQuality ValidateMove(Move move, Variation? bestVariation, Variation? runnerVariation)
+    {
+        MoveQuality moveQuality = MoveQuality.Unknown;
+
+        if (move.Evaluation == null || bestVariation == null || bestVariation.Evaluation == null)
+        {
+            return MoveQuality.Unknown;
+        }
+
+        if (runnerVariation == null || runnerVariation.Evaluation == null)
+        {
+            return MoveQuality.Only;
+        }
+
+        if (bestVariation != null)
+        {
+            if (move.EngineMove == bestVariation.Moves.FirstOrDefault()?.EngineMove)
+            {
+                return MoveQuality.Best;
+            }
+
+            double diff = Math.Abs(move.Evaluation.ChartScore() - bestVariation.Evaluation.ChartScore());
+            // double runnerDiff = Math.Abs(move.Evaluation.ChartScore() - runnerVariation.Evaluation.ChartScore());
+
+            if (move.EngineMove == runnerVariation.Moves.FirstOrDefault()?.EngineMove)
+            {
+                if (diff < 0.5)
+                {
+                    return MoveQuality.Runner;
+                }
+            }
+
+            if (diff > 1)
+            {
+                return MoveQuality.Blunder;
+            }
+
+            if (diff >= 0.5)
+            {
+                return MoveQuality.Questionmark;
+            }
+
+            return MoveQuality.Clubhouse;
+
+        }
+        return moveQuality;
+    }
+
+    private async Task Analyse()
+    {
+        int i = 0;
+        if (Analysis != null)
+        {
+            _chart.data.labels = Analysis.Game.State.Moves.Select(s => s.HalfMoveNumber.ToString()).ToList();
+            chart?.UpdateLabels(_chart.data.labels);
+            Analyzing = true;
+            await InvokeAsync(() => StateHasChanged());
+            cts = new CancellationTokenSource();
+            ReviewVariations = new Dictionary<int, List<Variation>>();
+            Analysis.Game.ReviewVariations = new Dictionary<int, List<Variation>>();
+
+            try
+            {
+                await foreach (var variation in Analysis.Analyze(cts.Token, TimeSpan.FromMilliseconds(reviewSettings.MsPerMove), 2, reviewSettings.Threads))
+                {
+                    if (!ReviewVariations.ContainsKey(variation.StartMove))
+                    {
+                        ReviewVariations[variation.StartMove] = new List<Variation>();
+                    }
+                    ReviewVariations[variation.StartMove].Add(variation);
+                    
+                    i++;
+                    if (i % 20 == 0)
+                    {
+                        UpdateChart();
+                    }
+                }
+            }
+            catch (OperationCanceledException) { }
+            finally
+            {
+                UpdateChart();
+                Analyzing = false;
+
+                foreach (var ent in ReviewVariations.OrderBy(o => o.Key))
+                {
+                    Analysis.Game.ReviewVariations[ent.Key] = ent.Value.OrderBy(o => o.Pv).ToList();
+                    if (ent.Key - 1 >= 0)
+                    {
+                        var move = Analysis.Game.State.Moves[ent.Key - 1];
+                        var eval = Analysis.Game.ReviewVariations[ent.Key].First().Evaluation;
+                        move.Evaluation = eval;
+                        if (eval != null)
+                        {
+                            eval.MoveQuality =
+                                ValidateMove(move, Analysis.Game.ReviewVariations[ent.Key - 1].FirstOrDefault(), Analysis.Game.ReviewVariations[ent.Key - 1].Count > 1 ? Analysis.Game.ReviewVariations[ent.Key - 1][1] : null);
+                        }
+                    }
+                }
+                await InvokeAsync(() => StateHasChanged());
+            }
+        }
+    }
+
+    private void ShowLine(int i)
+    {
+        if (Analysis != null)
+        {
+            int startMoveId = Analysis.Game.ObserverState.CurrentMove == null ? 0 : Analysis.Game.ObserverState.CurrentMove.HalfMoveNumber;
+            if (Analysis.Game.ReviewVariations.ContainsKey(startMoveId))
+            {
+                var variation = Analysis.Game.ReviewVariations[startMoveId].ElementAt(i);
+                if (variation != null)
+                {
+                    Analysis.Game.CreateVariation(startMoveId, variation.Moves.Select(s => s.EngineMove).ToList(), variation.Evaluation);
+                }
+            }
+        }
+    }
+
+    private async void Save()
+    {
+        if (Analysis != null)
+        {
+            await dbService.SaveGame(Analysis.Game, GameId);
+        }
+    }
+
+    private async Task MoveRequested()
+    {
+        if (engineComponent != null)
+        {
+            var move = await engineComponent.ExecuteBestMove();
+            if (move != null)
+            {
+                boardContainer?.DrawHints(new List<EngineMove>(), true);
+                boardContainer?.DrawOtherHints(new List<EngineMove>(), true);
+                await InvokeAsync(() => StateHasChanged());
+            }
+        }
+    }
+
+    private void StartEngineVsEngineGame()
+    {
+        if (Analysis != null)
+        {
+            Game eveGame = new Game();
+            for (int i = 0; i < Analysis.Game.ObserverState.Moves.Count; i++)
+            {
+                eveGame.Move(Analysis.Game.ObserverState.Moves[i].EngineMove);
+            }
+            engineService.CreateEngineGame(eveGame, "Test1");
+            _nav.NavigateTo("enginevsengine");
+        }
+    }
+
+    private void Stop()
+    {
+        cts.Cancel();
+    }
+
+    public void Dispose()
+    {
+        cts.Cancel();
+    }
+}
+
