5147166a88f1c6e5dbd7b96262792d0be3c12d2b
ipax77 <ph.ilipp@web.de>
2026-03-14 12:19:02 +0100

    add EngineGameComponent

---
+++ src/pax.BlazorChess.Board/EngineGameSession.cs
@@ -0,0 +1,20 @@
+﻿using pax.chess;
+using pax.uciChessEngine;
+
+namespace pax.BlazorChess.Board;
+
+public sealed class EngineGameSession
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Name { get; init; } = string.Empty;
+    public EngineGame EngineGame { get; init; } = default!;
+    public ChessGame Game { get; init; } = default!;
+    public ChessClock Clock { get; init; } = default!;
+    public ClockComponent? ClockComponent { get; set; }
+    public BoardComponent? BoardComponent { get; set; }
+    public bool DisplayAttached { get; set; }
+    public bool IsRunning { get; set; }
+    public string WhiteEngine { get; init; } = string.Empty;
+    public string BlackEngine { get; init; } = string.Empty;
+    public string? Result { get; set; }
+}
